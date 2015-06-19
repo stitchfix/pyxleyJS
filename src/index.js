d55c2423a0e3c639174a126fb8815d3a82453ff9
Nicholas Kridler <nkridler@Nicholas-Kridlers-MacBook-Pro.local>
2015-06-19 14:15:20 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 
-var SelectButton = require('./buttonFilters').SelectButton;
-var ConditionalSelectButton = require('./buttonFilters').ConditionalSelectButton;
-var ApiButton = require('./buttonFilters').ApiButton;
-var DownloadButton = require('./buttonFilters').DownloadButton;
-var LinePlot = require('./linePlot').LinePlot;
-var Datamaps = require('./datamap').Datamaps;
 var FilterChart = require('./chartFactory').FilterChart;
 var Filter = require('./filterFactory').Filter;
 var Chart = require('./chartFactory').Chart;
-var Table = require('./table').Table;
 
 export default {
     Filter,
     Chart,
-    FilterChart,
-    SelectButton,
-    ConditionalSelectButton,
-    ApiButton,
-    DownloadButton,
-    LinePlot,
-    Datamaps,
-    Table
+    FilterChart
 };
 
 
