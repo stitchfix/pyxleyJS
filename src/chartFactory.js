acc6201b3d59af48ba87759bb6d1d2690132e590
Nicholas Kridler <nkridler@Nicholas-Kridlers-MacBook-Pro.local>
2015-07-01 13:37:10 -0700

    fixes to datatable; updated index; moved FilterChart to layouts

--- src/chartFactory.js
+++ src/chartFactory.js
@@ -1,9 +1,6 @@
 
 var LinePlot = require('./linePlot').LinePlot;
 var Table = require('./table').Table;
-var Filter = require('./filterFactory').Filter;
-var Row = ReactBootstrap.Row;
-var Button = ReactBootstrap.Button;
 var Datamaps = require('./datamap').Datamaps;
 var MetricsGraphics = require('./metricsgraphics').MetricsGraphics;
 
@@ -39,62 +36,6 @@ const Chart = React.createClass({
     }
 });
 
+module.exports.Chart = Chart;
 
-const FilterChart = React.createClass({
-    getDefaultProps: function() {
-        return {
-            filters: React.PropTypes.array,
-            filter_style: React.PropTypes.string,
-            charts: React.PropTypes.array,
-            dynamic: React.PropTypes.string
-        };
-    },
-    _handleClick: function(input) {
-        var params = {};
-        for(var i = 0; i < this.props.filters.length; i++){
-            var vals = this.refs["filter_".concat(i)].refs.filter.getCurrentState();
-            for(var key in vals){
-                params[key] = vals[key];
-            }
-        }
-        if(input){
-            for(var i = 0; i < input.length; i++){
-                params[input[i].alias] = input[i].value;
-            }
-        }
-        for(var i = 0; i < this.props.charts.length; i++){
-            this.refs["chart_".concat(i)].update(params);
-        }
-        return params;
-    },
-    render: function(){
-        var items = this.props.filters.map(function(x, index){
-            return(<Filter
-                ref={"filter_".concat(index)}
-                onChange={this._handleClick}
-                dynamic={this.props.dynamic}
-                type={x.type} options={x.options}/>);
-        }.bind(this));
 
-        var charts = this.props.charts.map(function(x, index){
-            return(<Chart
-                ref={"chart_".concat(index)}
-                type={x.type} options={x.options}/>);
-        });
-        return (
-            <div>
-                <Row>
-                <div className={this.props.filter_style}>
-                {this.props.dynamic ? null : <Button onClick={this._handleClick} >Update!</Button>}
-                {items}
-                </div>
-                </Row>
-                <Row>
-                {charts}
-                </Row>
-            </div>
-            );
-    }
-});
-
-module.exports.FilterChart = FilterChart;
--- src/table.js
+++ src/table.js
@@ -4,15 +4,11 @@ var Table = React.createClass({
     getDefaultProps: function() {
         return {
             options: {
-                leftFixed: 0,
-                rightFixed: 0,
                 table_options: {
                     deferRender: true,
                     paging: false,
                     scrollX: true,
-                    bSort: false,
-                    "sDom": '<"top">rt<"bottom"lp><"clear">'
-                },
+                    bSort: false,                },
                 id: "mytable",
                 url: "",
                 params: {}
@@ -29,24 +25,33 @@ var Table = React.createClass({
     },
     _initialize: function() {
 
-        $.get(this.props.options.url.concat("?",$.param(this.props.options.params)), function(result){
-            this.setState({columns: result.columns});
-            var options = this.props.options.table_options;
-            options.data = result.data;
-            options.columns = result.columns;
-            options.aaData = result.data;
-            var table = $('#'.concat(this.props.options.id)).dataTable(options);
-            if( (this.props.options.leftFixed + this.props.options.rightFixed) > 0 ){
-                new $.fn.dataTable.FixedColumns( table, {
-                    leftColumns: this.props.options.leftFixed,
-                    rightColumns: this.props.options.rightFixed
-                });
-            };
-        }.bind(this));
+        $.get(this.props.options.url.concat("?",$.param(this.props.options.params)),
+            function(result){
+                this.setState({columns: result.columns});
+                var options = this.props.options.table_options;
+                options.data = result.data;
+                options.columns = result.columns;
+                options.aaData = result.data;
+                if("columnDefs" in this.props.options.table_options){
+                    options.columnDefs = this.props.options.table_options.columnDefs;
+                    options.columnDefs.forEach(function(x){
+                        x.render = new Function("return '" + x.render + "';");
+                    });
+                }
+
+                options.initComplete = new Function("settings", "json",
+                    this.props.options.table_options.initComplete);
+                options.drawCallback = new Function("settings",
+                    this.props.options.table_options.drawCallback);
+                $('#'.concat(this.props.options.id)).dataTable(options);
+            }.bind(this)
+        );
+
     },
     _update: function(params) {
-        var _table = $('#'.concat(this.props.options.id)).DataTable();
+        var _table = $('#'.concat(this.props.options.id)).dataTable();
         _table.ajax.url(this.props.options.url.concat("?",$.param(params))).load();
+
     },
     render: function() {
         var header = this.state.columns.map(function(item, index) {
@@ -56,7 +61,10 @@ var Table = React.createClass({
         });
         return (
             <div>
-                <table id={this.props.options.id} className="display" cellSpacing="0" width="100%">
+                <table id={this.props.options.id}
+                    className={this.props.options.className}
+                    cellPadding="0"
+                    cellSpacing="0" width="100%">
                 <thead><tr>{header}</tr></thead>
                 </table>
             </div>
