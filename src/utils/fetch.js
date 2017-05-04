fe854ed6309112510eb5448dc8ac1e7a0748a351
Nicholas Kridler <nkridler@nmkridler.local>
2017-05-04 14:51:44 -0700

    added a function for api requests using fetch

---
+++ src/utils/fetch.js
@@ -0,0 +1,27 @@
+import fetch from 'isomorphic-fetch';
+import {stringify} from 'query-string';
+
+const FETCH_INIT = {
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    redirect: 'manual'
+}
+
+export const fetchData = (
+    path,
+    args,
+    onSuccess,
+    onError
+) => (
+    fetch(`${ path }?${ stringify(args) }`, FETCH_INIT)
+        .then(response => {
+            if(response.ok) {
+                response.json().then(onSuccess)
+            } else if (response.type == 'opaqueredirect') {
+                window.location.reload()
+            } else {
+                onError()
+            }
+        })
+        .catch(onError)
+)
